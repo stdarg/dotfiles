e3f607b8db94a15cec1bdf0b150c07119096770e
Edmond Meinfelder <edmond@arceoanalytics.com>
2019-01-29 08:47:44 -0800

    Fix ln cmd

--- setup_links.sh
+++ setup_links.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+CWD=`pwd`
 if [ -f ~/.bashrc ]
 then
     mv ~/.bashrc ~/.bashrc.bak
 fi
-ln -s bashrc.sh ~/.bashrc
+ln -s ${CWD}/bashrc.sh ~/.bashrc
+
+if [ -f ~/.gitconfig ]
+then
+    mv ~/.gitconfig ~/.gitconfig.bak
+fi
+ln -s ${CWD}/gitconfig.ini ~/.gitconfig
 
