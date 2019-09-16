18bed79cfa93b2d0ae60a38129fc944ff1db4ae7
Edmond Meinfelder <edmond@arceoanalytics.com>
2019-09-16 07:10:09 -0700

    Larger history

--- bash_env.sh
+++ bash_env.sh
@@ -1,13 +1,14 @@
 # don't put duplicate lines or lines starting with space in the history.
 # See bash(1) for more options
-HISTCONTROL=ignoreboth
+# HISTCONTROL=ignoreboth
+HISTTIMEFORMAT="%m/%d/%y %T "
 
 # append to the history file, don't overwrite it
 shopt -s histappend
 
 # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
-HISTSIZE=1000
-HISTFILESIZE=2000
+HISTSIZE=9000
+HISTFILESIZE=90000
 
 # for run-tests.sh script
 if [ -f /usr/bin/vim ]
