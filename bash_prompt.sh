481a5ae876ee3bb4fa87dea8764395a67eec4109
Edmond Meinfelder <edmond@Edmonds-MacBook-Pro.local>
2022-04-04 16:37:48 -0700

    Assume brew install

--- bash_prompt.sh
+++ bash_prompt.sh
@@ -7,12 +7,11 @@ GIT_PROMPT_ONLY_IN_REPO=0
 GIT_PROMPT_THEME=Solarized
 GIT_PROMPT_START="${WHITE}\w${DEFAULT}"
 GIT_PROMPT_END=" ${RED}\$${DEFAULT} "
-__GIT_PROMPT_DIR=${HOME}/.bash-git-prompt
-GIT_PROMPT_SH=${__GIT_PROMPT_DIR}/gitprompt.sh
 
-if [ -f ${GIT_PROMPT_SH} ]
+if [ -f "/opt/homebrew/opt/bash-git-prompt/share/gitprompt.sh" ]
 then
-    source ${GIT_PROMPT_SH}
+    __GIT_PROMPT_DIR="/opt/homebrew/opt/bash-git-prompt/share"
+    source "/opt/homebrew/opt/bash-git-prompt/share/gitprompt.sh"
 else
     echo "bash-git-prompt is not installed, please install it."
     echo "see: https://github.com/magicmonty/bash-git-prompt#installation"
